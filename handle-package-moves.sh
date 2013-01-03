93b64acf7ad42f0139edd7fa90e26306cb62375a
Fabio Erculiani <lxnay@sabayon.org>
2013-01-03 08:59:18 +0000

    [*] simplify handle-package-moves.sh a lot, do not try be smart when it's not possible

--- handle-package-moves.sh
+++ handle-package-moves.sh
@@ -10,12 +10,9 @@
 # (C) 2012 Sławomir Nizio <slawomir.nizio#sabayon.org>
 # this scripty is placed in public domain
 
-# path to the glourious script or name if in PATH
-modify_script=${modify_script:-modify.pl}
-
 # for example: particles.git/weekly
 # default value is useful when it's run from particles.git directory
-particles_dir=${particles_dir:-weekly}
+particles_dir=${particles_dir:-.}
 
 # don't edit this
 available_after_move=()
@@ -27,74 +24,25 @@ if [[ $# -eq 0 ]]; then
 fi
 
 start() {
-	if ! "$modify_script" --help > /dev/null; then
-		echo "Looks that I can't execute $modify_script." >&2
-		echo "Check if you have that script and adjust \$PATH" >&2
-		echo "or \$modify_script if needed."
-		exit 1
-	fi
-	if ! cd "$particles_dir"; then
-		echo "cd $particles_dir = nope" >&2
-		exit 1
-	fi
 	for file in "$@"; do
 		readfileanddostuff "$file" || { echo "aborted"; exit 1; }
 	done
-	if [[ ${#available_after_move[@]} -gt 0 ]]; then
-		# Here it can print invalid package names if a package was
-		# renamed multiple times, or just removed since.
-		echo
-		echo "new candidates"
-		echo "These are packages that weren't present in a particle before"
-		echo "their rename, and a particle for their category exists."
-		echo "Displaying them in case it's useful (whitespace separated)."
-		echo "Check if they refer to packages that are really available!"
-		echo "${available_after_move[@]}"
-	else
-		echo
-		echo "No new candidates."
-	fi
 }
 
 pkgmove() {
-	local old="${1}"
-	local new="${2}"
+	local target="${1}"
+	local old="${2}"
+	local new="${3}"
 
-	echo "@@@ $old -> $new"
-	oldcat=${old%/*}
-	newcat=${new%/*}
-	# echo "<$oldcat,$newcat>"
-	if [[ ! -f $oldcat.particle ]]; then
-		echo "no-file-old $oldcat.particle, skipping"
-		continue
-	fi
-
-	# modify.pl currently returns success when no entry to be deleted
-	# so we use this to know if new (replaced) entry should be added
-	# or not
-	if egrep -q "^\s*$old(,|$)" "$oldcat.particle"; then
-		# removed even if new name won't be added
-		"$modify_script" --noask --delete "$oldcat.particle" "$old"
-		was_present=1
-	else
-		was_present=0
-	fi
-
-	if [[ ! -f $newcat.particle ]]; then
-		echo "no-file-new $newcat.particle, skipping the rest"
-		continue
-	fi
-
-	if [[ $was_present = 1 ]]; then
-		"$modify_script" --noask "$newcat.particle" "$new"
-	else
-		# if it wasn't there before (and it's not now) and a particle
-		# for this category exists (checked above), inform about it
-		# maybe someone would want to add it
-		if ! egrep -q "^\s*$new(,|$)" "$newcat.particle"; then
-			available_after_move+=( "$new" )
-		fi
-	fi
+	echo "@@@ pkgmove: $target, $old -> $new"
+	# handle app-foo/bar,
+	sed -i "s:$old,:$new,:g" "$target" || return 1
+	# handle app-foo/bar:
+	sed -i "s;$old:;$new:;g" "$target" || return 1
+	# handle app-foo/bar$
+	sed -i "s;${old}$;$new;g" "$target" || return 1
+	# TODO: handle versioned <app-foo/bar-9999 without
+	# rewriting wrong PN. app-foo/bar-baz-9999 != app-foo/bar-9999.
 }
 
 readfileanddostuff() {
@@ -107,13 +55,15 @@ readfileanddostuff() {
 		echo "$file doesn't exist or can't be read, or is invalid type" >&2
 		return 1
 	fi
-	while read op old new; do
-		if [[ "$op" = "move" ]]; then
-			pkgmove "${old}" "${new}"
-		elif [[ "$op" = "slotmove" ]]; then
-			echo "slotmove not supported yet, line: ${op} ${old} ${new}"
-		fi
-	done < "$file"
+	for particle in $(find "${particles_dir}" -name "*.particle" | sort); do
+		while read op old new; do
+			if [[ "$op" = "move" ]]; then
+				pkgmove "$particle" "$old" "$new"
+			elif [[ "$op" = "slotmove" ]]; then
+				echo "slotmove not supported yet, line: $op $old $new"
+			fi
+		done < "$file"
+	done
 }
 
 start "$@"
