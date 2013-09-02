5cd7d6158b880d7cd424a20ecb9698b624b7a5c8
Fabio Erculiani <lxnay@sabayon.org>
2013-09-02 10:32:46 +0200

    [hooks] empty post.sh for now

--- hooks/post.sh
+++ hooks/post.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-world=/var/lib/portage/world
+#world=/var/lib/portage/world
 
-echo "Zeroing the world file ${world}"
+#echo "Zeroing the world file ${world}"
 
-echo > ${world}
+#echo > ${world}
