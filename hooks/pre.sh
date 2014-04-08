33f4d1a0830cfbfc16d0b2262d13505d53cfad47
Fabio Erculiani <lxnay@sabayon.org>
2014-04-08 11:57:38 +0200

    Move sabayon-limbo packages to sabayonlinux.org automatically

--- hooks/pre.sh
+++ hooks/pre.sh
@@ -4,3 +4,8 @@ world=/var/lib/portage/world
 
 # echo "Generating the world file at ${world}"
 # qlist -ICS > "${world}"
+
+# Move sabayon-limbo to sabayonlinux.org
+EIT_NO_RESOURCES_LOCK=1 eit mv sabayon-limbo sabayonlinux.org --quick
+
+exit 0
